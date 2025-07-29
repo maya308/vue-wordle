56f0e4d79901d9c753599c082bdd1aa0d8a0ca9a
Maya Moktan <mayamoktan308@gmail.com>
2025-07-28 23:49:49 -0400

    title and href

--- src/Game.vue
+++ src/Game.vue
@@ -188,10 +188,10 @@ function genResultGrid() {
     </div>
   </Transition>
   <header>
-    <h1>VVORDLE</h1>
+    <h1>ZAINA BABY WORDLE</h1>
     <a
       id="source-link"
-      href="https://github.com/yyx990803/vue-wordle"
+      href="https://github.com/mayamoktan308/zaina-baby-name-wordle"
       target="_blank"
       >Source</a
     >
